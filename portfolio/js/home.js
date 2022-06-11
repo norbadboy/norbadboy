271dfa3fe7eb4ee7ad40c1cad08bffa87a73d8f5
norbadboy <norbadboy@gmail.com>
2022-06-11 15:25:18 +0200

    moved index out of folder

--- portfolio/js/home.js
+++ portfolio/js/home.js
@@ -1,7 +1,5 @@
 const buttonBackToIntro = document.getElementById("button--back-to-intro");
 
-
 buttonBackToIntro.onclick = function () {
-  location.href = "/portfolio/index.html";
+  location.href = "/index.html";
 };
-
